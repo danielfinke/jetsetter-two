4796608fa26e41d6f5ec415caf331266d537d62b
Daniel Finke <danielfinke2011@gmail.com>
2021-05-21 16:33:27 -0700

    Implement most of ItemStore (@observable, @computed, @action.bound)

--- src/store/ItemStore.js
+++ src/store/ItemStore.js
@@ -1,4 +1,41 @@
 import { observable, computed, action } from 'mobx';
 import Item from './ItemModel';
 
-export default class ItemStore {}
+export default class ItemStore {
+  @observable items = [];
+  @observable packedItemsFilter = '';
+  @observable unpackedItemsFilter = '';
+
+  @computed get packedItems() {
+    return this.items.filter((item) => item.packed);
+  }
+
+  @computed get unpackedItems() {
+    return this.items.filter((item) => item.unpacked);
+  }
+
+  @computed get filteredPackedList() {
+    return this.packedItems().filter((item) =>
+      item.value.includes(this.packedItemsFilter),
+    );
+  }
+
+  @computed get filteredUnpackedList() {
+    return this.unpackedItems().filter((item) =>
+      item.value.includes(this.unpackedItemsFilter),
+    );
+  }
+
+  @action.bound
+  addItem() {}
+
+  @action.bound
+  updatePackedItemsFilter(filter) {
+    this.packedItemsFilter = filter;
+  }
+
+  @action.bound
+  updateUnpackedItemsFilter(filter) {
+    this.unpackedItemsFilter = filter;
+  }
+}
